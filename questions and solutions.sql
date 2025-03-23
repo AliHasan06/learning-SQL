da692442b009e92d6ffa98e3afbdd4cf60fa241a
Alihasan06 <alihassanraja06@gmail.com>
2025-03-23 21:04:22 +0500

    Solved my first SQL question

---
+++ questions and solutions.sql
@@ -0,0 +1,19 @@
+-- create a db for xyz company 
+-- step 1 create a table inside this db to store employee info(id , name , salary) 
+-- step2  add following info in db:
+ -- 1 , 'adam' , 25000 
+--  2 , 'bob' , 30000
+--  3 , casey , 40000
+--  step 3 : select and views all table data 
+
+CREATE DATABASE XYZCORP;
+USE XYZCORP;
+CREATE TABLE EMPLOYEEINFO(
+ID INT PRIMARY KEY,
+NAME VARCHAR(50),
+SALARY INT
+);
+INSERT INTO EMPLOYEEINFO(ID, NAME, SALARY) VALUES(  3 , 'casey' , 40000
+),(2 , 'bob' , 30000),(1 , 'adam' , 25000);
+SELECT * FROM EMPLOYEEINFO;
+SHOW TABLES;
