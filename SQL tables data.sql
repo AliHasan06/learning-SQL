c2c620311aab011360d5860e4a1f8ac6e2203623
Alihasan06 <alihassanraja06@gmail.com>
2025-03-27 02:01:40 +0500

    constraints

--- SQL tables data.sql
+++ SQL tables data.sql
@@ -2,11 +2,21 @@ CREATE DATABASE college;
 
 use college;
 create table student(
-rollno INT primary key,
-name varchar(50)
+rollno INT ,
+name varchar(50),
+primary key(rollno,name)
 );
-insert into student(rollno,name) values(1,'ahmed'),(2,'ali'),(3,'aman');
+insert into student(rollno,name) values(6,'ali'),(3,'ali'),(1,'ali'),(1,'aman');
 SELECT * from student;
 show tables;
+create table temp1(
+id int unique
+);
+insert into temp1 values(1);
+insert into temp1 values(1);
+SELECT * from temp1;
+SELECT * from student;
+
 
 drop table student;
+drop table temp1;
