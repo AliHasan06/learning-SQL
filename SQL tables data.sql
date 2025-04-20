55bd7f1a809dcd3271f1fbafd004ff1862512cd4
Alihasan06 <alihassanraja06@gmail.com>
2025-04-20 22:19:51 +0500

    "Added student table with data, created employee_info table in new database xyz, and removed temp1 table."

--- SQL tables data.sql
+++ SQL tables data.sql
@@ -2,14 +2,29 @@ CREATE DATABASE college;
 
 use college;
 show tables;
-create table temp1(
+create table student(
 id int unique
+,name varchar(50)
 );
 insert into temp1 values(1);
-insert into temp1 values(1);
+insert into student values(1,"Hasan"),(3,"ahsan");
 SELECT * from temp1;
 SELECT * from student;
 
 
 drop table student;
 drop table temp1;
+
+
+
+
+
+
+create database xyz;
+use xyz;
+create table employee_info(
+id int primary key,
+name varchar(50),
+salary int
+);
+insert into employee_info  values(1,"adam",25000),(2,"bob",30000),(3,"casey",40000);
